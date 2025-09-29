629c47d7b8467ffd1966048f23713b258ead0c2d
Satvik12221as <sanu8800971634@gmail.com>
2025-09-29 23:01:12 +0530

    wind speed better sliding

--- weatherprediction/app.py
+++ weatherprediction/app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import joblib
+import os
 import numpy as np
 
 # Load models
-reg_model = joblib.load("models/temperature_regressor.pkl")
-clf_model = joblib.load("models/weather_classifier.pkl")
-le = joblib.load("models/label_encoder.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+reg_model = joblib.load(os.path.join(BASE_DIR, "models", "temperature_regressor.pkl"))
+clf_model = joblib.load(os.path.join(BASE_DIR, "models", "weather_classifier.pkl"))
+le = joblib.load(os.path.join(BASE_DIR, "models", "label_encoder.pkl"))
+
 
 # Streamlit UI
 st.title("🌤️ Weather Prediction App")
@@ -13,7 +17,7 @@ st.markdown("Enter today's data to predict temperature and weather condition:")
 
 # User Inputs
 humidity = st.slider("Humidity (%)", 0, 100, 70)
-wind_speed = st.slider("Wind Speed (km/h)", 0.0, 15.0, 3.5)
+wind_speed = st.slider("Wind Speed (km/h)", 0.0, 15.0, 3.5 ,step=0.1)
 mean_pressure = st.slider("Mean Pressure", 950, 1100, 1010)
 
 if st.button("Predict Weather"):
